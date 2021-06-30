c741341b371e1616d702660f55d723631ece05ec
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-06-30 18:22:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 
+ $(window).on("load",function(){
+  $(".loading").fadeOut(2000);
+});
+
  AOS.init();
- 
+
 $("#bar").click(function(){
     $("#navslide").slideToggle();
   });
+
+
+
   $(".links").click(function(){
     $("#navslide").slideToggle();
   });
 
-  $(window).on("load",function(){
-    $(".loading").fadeOut(2000);
+
+
+  $({ Counter: 0 }).animate({
+    Counter: $('.trainer').text()
+  }, {
+    duration: 2000,
+    easing: 'swing',
+    step: function() {
+      $('.trainer').text(Math.ceil(this.Counter));
+    }
   });
+
+  $({ Counter: 0 }).animate({
+    Counter: $('.member').text()
+  }, {
+    duration: 2000,
+    easing: 'swing',
+    step: function() {
+      $('.member').text(Math.ceil(this.Counter));
+    }
+  });
+
